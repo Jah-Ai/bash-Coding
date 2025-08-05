b7db81b31abdb9cec895065fb2ec0cd2d1cf9683
Jah-Ai <batteringmoms@gmail.com>
2025-08-05 13:49:10 -0400

    Add bash scripts demonstrating basic constructs

---
+++ bash-case.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Enter a number between 1 and 3:"
+read num
+
+case $num in
+	1)
+		echo "You entered one";;
+	2)
+		echo "You entered two";;
+	3)
+		echo "You entered three";;
+	*)
+		echo "Invalid input";;
+esac
---
+++ bash-if.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+number=5
+
+if [ $number -gt 3 ]; then
+	echo "Number is greater than 3"
+else
+	echo "Number is 3 or less"
+fi
